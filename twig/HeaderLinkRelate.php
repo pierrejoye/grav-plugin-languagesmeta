d64a99bbb3a45ff028813e44413577a11fd57e11
Pierre Joye <pierre.php@gmail.com>
2021-01-30 08:16:31 +0700

    initial import

---
+++ twig/HeaderLinkRelate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Grav\Plugin;
+use \Grav\Common\Grav;
+
+class HeaderLinkRelate extends \Twig_Extension
+{
+    private $grav;
+
+    public function __construct($grav) {
+        $this->grav = $grav;
+    }
+
+    public function getName()
+    {
+        return 'HeaderLinkRelate';
+    }
+
+    public function getFunctions()
+    {
+        
+        return [
+            new \Twig_SimpleFunction('headerLinkRelate', [$this, 'headerLinkRelate'])
+        ];
+    }
+
+    public function headerLinkRelate($page, $collection)
+    {
+        $language = $this->grav['language'];
+        $defaultLanguage = $language->getDefault();
+        $translatedPages = $page->translatedLanguages(true);
+        $alternates = '';
+        foreach ($translatedPages as $language => $route) {
+            $alternates .=  '<link rel="alternate" href="/' . $language . $route . ( $defaultLanguage == $language ?  '" hreflang="x-default"':'"') . '/>' . PHP_EOL;
+        }
+        return $alternates;
+    }
+}
